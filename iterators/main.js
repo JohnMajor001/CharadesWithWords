a0415a1067c2c84fc7bbe97dd6071c090d6e5a1d
John David Thomas Major <johnmajor@Johns-Mac-mini.local>
2018-03-28 14:46:46 +0100

    Adding correct answer and Pass option

--- iterators/main.js
+++ iterators/main.js
@@ -1,35 +1,38 @@
 window.onload = function() {
   /* Main JS */
-  // VARIABLES
-  var object = ["Cane", "Kite", "Computer", "Television", "Car"];
-  var game = document.getElementById('game');
-  var word = document.getElementById('wordToDescribe');
-  var timer = document.getElementById('timer');
-  var startButton = document.getElementById('start');
-  var count = 0;
-  var numberForTimer = 30;
-  var timesUpMessage = "Time's up!";
+
   // things to keep track of
   // number of passes
   // number of answers done correctly
   // show review at the end of each turn?
-  // Timer! Countdown
+  // Timer! Countdown                                                       DONE
   // options to change - countdown timer, THEME!!!! big one.
-  // Where players are - like their counter piece on a board
-  startButton.addEventListener("click", stopWatch); // test can you add multiple functions to this?
-function stopWatch() {
+  // Where players are - their counter piece on a board
+  startButton.addEventListener("click", round); // test can you add multiple functions to this?
+function round() {
   timer.innerHTML = "Go!";
   setInterval(innerStopWatch, 1000);
+  gotIt.innerHTML = "<button class='button' id='correctAnswer'>Got it!</button>";
+  pass.innerHTML = "<button class='button' id='passed'>Pass</button>";
+  var correctAnswer = document.getElementById('correctAnswer');
+  var passed = document.getElementById('passed');
+  correctAnswer.addEventListener("click", correctlyAnswered);
+  passed.addEventListener("click", passedWord);
   }
   function innerStopWatch() {
-    if(count == 30) {
+    if(numberForTimer == 0) {
       timer.innerHTML = timesUpMessage;
       return;
     } else {
       timer.innerHTML = numberForTimer;
-      count++;
       numberForTimer--;
     }
+  }
+  function correctlyAnswered() {
+    alert("hi there!");
+  }
+  function passedWord() {
+    alert("hi there!");
   }
 
 
---
+++ iterators/variables.js
@@ -0,0 +1,9 @@
+var object = ["Cane", "Kite", "Computer", "Television", "Car"];
+var game = document.getElementById('game');
+var word = document.getElementById('wordToDescribe');
+var timer = document.getElementById('timer');
+var startButton = document.getElementById('start');
+var gotIt = document.getElementById('gotIt');
+var pass = document.getElementById('pass')
+var numberForTimer = 30;
+var timesUpMessage = "Time's up!";
