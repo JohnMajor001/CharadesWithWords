e94d1cad8ca8376facadc81c0f52afcde6a8d135
John David Thomas Major <johnmajor@Johns-Mac-mini.local>
2018-03-28 15:14:39 +0100

    pass and got it button working

--- iterators/main.js
+++ iterators/main.js
@@ -8,10 +8,22 @@ window.onload = function() {
   // Timer! Countdown                                                       DONE
   // options to change - countdown timer, THEME!!!! big one.
   // Where players are - their counter piece on a board
+
+  // Start Button
   startButton.addEventListener("click", round); // test can you add multiple functions to this?
+
+  // Round Begins
 function round() {
   timer.innerHTML = "Go!";
   setInterval(innerStopWatch, 1000);
+
+  // Generate Word to Describe
+  var objectLengthForRand = object.length;
+  var rando = Math.floor(Math.random() * objectLengthForRand);
+  var randomObject = object[rando];
+  word.innerHTML = `Describe: ${randomObject}`;
+  object.splice(rando, 1);
+
   gotIt.innerHTML = "<button class='button' id='correctAnswer'>Got it!</button>";
   pass.innerHTML = "<button class='button' id='passed'>Pass</button>";
   var correctAnswer = document.getElementById('correctAnswer');
@@ -29,10 +41,34 @@ function round() {
     }
   }
   function correctlyAnswered() {
-    alert("hi there!");
+    if(object.length == 0) {
+      alert("You've run out of options! Oops");
+      return;
+    } else {
+      var objectLengthForRand = object.length - 1;
+      var rando = Math.floor(Math.random() * objectLengthForRand);
+      var randomObject = object[rando];
+      word.innerHTML = `Describe: ${randomObject}`;
+      object.splice(rando, 1);
+    }
   }
   function passedWord() {
-    alert("hi there!");
+    if(object.length == 0) {
+      alert("You've run out of options! Oops");
+      return;
+    } else {
+      if(noOfPasses == 3) {
+        alert("You have no more passes")
+        return;
+      } else {
+        var objectLengthForRand = object.length - 1;
+        var rando = Math.floor(Math.random() * objectLengthForRand);
+        var randomObject = object[rando];
+        word.innerHTML = `Describe: ${randomObject}`;
+        object.splice(rando, 1);
+        noOfPasses++;
+      }
+    }
   }
 
 
--- iterators/variables.js
+++ iterators/variables.js
@@ -1,4 +1,5 @@
 var object = ["Cane", "Kite", "Computer", "Television", "Car"];
+var arrayToCheckBy = [];
 var game = document.getElementById('game');
 var word = document.getElementById('wordToDescribe');
 var timer = document.getElementById('timer');
@@ -7,3 +8,4 @@ var gotIt = document.getElementById('gotIt');
 var pass = document.getElementById('pass')
 var numberForTimer = 30;
 var timesUpMessage = "Time's up!";
+var noOfPasses = 0;
