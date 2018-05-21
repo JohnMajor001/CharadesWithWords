bf9a4db392674f07524772bce0388548bed6d512
JohnMajor001 <35918539+JohnMajor001@users.noreply.github.com>
2018-05-21 19:31:00 +0100

    poop
    poop

---
+++ 12052018/Verbalise---forced-Settings-master/resources/js/disneyvariables.js
@@ -0,0 +1,142 @@
+var disney = {
+	name: 'Disney',
+	array: ['Snow White', 'Sleepy', 'Sneezy', 'Bashful', 'Dopey', 'Happy', 'Doc', 'Grumpy', 'Prince Charming', 'The 7 dwarves', 'Poison apple', 'True love’s kiss', 'Cinderella',
+			'Glass slipper', 'Gus', 'Ugly stepsister', 'Lucifer', 'Sleeping beauty', 'Aurora / Rose', 'Spinning wheel', 'Maleficient', 'Flora Merryweather', 'Fairy Godmother', 
+			'Ariel', 'King Triton', 'Flotsam and Jetsam', 'Sebastian', 'Eric', 'Flounder', 'Atlantis', 'Guppy', 'Mermaid', 'Pocahontas', 'Flick', 'Meeko', 'Percy the pug', 'Governor Ratcliffe',
+			'John smith', 'Savages', 'Grandmother willow', 'Aladdin', 'Jasmine', 'Iago', 'Jafar', 'Genie', 'Abu', 'The lamp', 'Rajah', 'Woody', 'Buzz', 'Jessie', 'Bull\'s eye', 'The Prospector',
+			'Al\'s Toy Barn', 'Sid', 'Andy', 'Mrs Nesbit', 'Lotso Hugs', 'Bo Peep', 'Wheezy', 'The claw', 'Mr Potato Head', 'Slinky', 'Ham', 'Rex', 'RC', 'Infinity And Beyond',
+			'Mulan', 'Fa Zhou', 'Li Shang', 'Grandmother Fa', 'Fa Li', 'Cri-Kee', 'Mushu', 'Shan Yu', 'Moana', 'Maui', 'Heihei', 'Tamatoa', 'Tiana', 'Prince Naveen', 'Raymond', 'Evangeline',
+			'King Louis', 'Dr. Facilier', 'Charlotte La Bouff', 'Mama Odie', 'Belle', 'Cogsworth', 'Lumiere', 'Chip', 'Mrs Pots', 'Gaston', 'Be our guest', 'Beauty and the Beast', 'Big Hero 6',
+			'Baymax', 'Hiro Hamada', 'Yokai', 'Mama Coco', 'Ernesto De la Cruz', 'Miguel Rivera', 'Dante', 'Princess Anna', 'P.T. Flea', 'Heimlich', 'Hopper', 'Wreck it Ralph', 'Fix it Felix',
+			'Going Turbo', 'Sergeant Calhoun', 'Lilo', 'Stitch', 'Experiment 626', 'Jumba Jookiba', 'Vanellope von Schweetz', 'King Candy', 'Nani Pelekai', 'Ohana', 'David Kawena', 'Baloo', 
+			'Bagheera', 'Shere Khan', 'Mowgli', 'Kaa', 'Hercules', 'Philoctetes', 'Hades', 'Zeus', 'Megara', 'Pegasus', 'Pain and Panic', 'A True Hero', 'The Fates', 'Titans', 'Rapunsel',
+			'Flynn Rider', 'Eugene Fitzherbert', 'Mike Wazowski', 'Sully', 'Code 2319', 'Randal', 'Boo', 'Abominable Snowman', 'Celia Mae', 'Mr Incredible', 'Violet', 'Dash', 'Syndrome',
+			'Mufasa', 'Simba', 'Nala', 'Timone and Pumba', 'Hakuna Matata', 'Minnie Mouse', 'Donald Duck', 'Goofy', 'Max', 'Micky Mouse', 'Ed', 'Pride Rock', 'Zazu', 'Elephant Graveyard',
+			'Bippity boppity boo', 'Basil of Bakerstreet', 'Professor Ratigan', 'Olivia Flaversham', 'Eezma', 'Krunk', 'Kuzco', 'Pacha', 'Bambi', 'Thumper', 'Gurgi', 'Princess Eilonwy', 
+			'Horned King', 'Daisy Duck', 'Pluto', 'Flower', 'Little April Shower', 'Peter Pan', 'Little April shower', 'Peter Pan', 'Wendy', 'Captain Hook', 'Jiminy Cricket', 'Powerline', 
+			'Pinocchio', 'Fantasia', 'Real boy', 'Dodger', 'Oliver and Company', 'Puppet', 'Fagin', 'Bill Sykes', 'Gepetto', 'Dory', 'Nemo', 'Clownfish', '42 Wallaby Way', 'Alice in Wonderland',
+			'Chesher Cat', 'Mad Hatter', 'Tweedledee & Tweedledum', 'Queen of Hearts', 'White Rabbit', 'Absolem', '' ],
+
+
+};
+
+Dory
+Nemo
+Clownfish
+42 wallaby way
+Bubbles
+Laid back Turtle
+
+Alice
+Wonderland
+Cat
+Caterpillar
+Tweedledum
+Tweedledee
+Queen of hearts
+White rabbit 
+
+Tarzan
+Jane
+I know no other names
+Son of man
+Strangers like me
+SUN 22:37
+Wall-e
+Robin Hood
+Prince John
+Friar tuck
+Tod (the fox) and the hound
+The girl fox - vixie?
+Thumbelina
+Norte dame
+Hunchback
+Esmeralda
+Gypsies
+Gargoyle
+Up
+Frozen
+Let it go
+Anna
+Hans 
+Mr weasleton 
+Snowman deer blonde guy
+Wee gnomes
+Brave
+Zootopia
+Kung fu panda
+Tinker bell
+Dumbo
+
+
+
+
+
+
+
+
+
+
+
+Marvel
+Thor
+Hammer
+Iron man
+Robert Downey jr
+Chris Evans
+Captain America
+Shield
+Vibranium
+Wakanda
+Black panther
+Martin freeman
+Goddess of death
+Ragnarok 
+Avengers
+Thannis 
+Green doll
+Starlord
+Raccoon
+Groot
+Infinity stones
+That place with the dwarf 
+Asgard 
+Loki 
+The hulk
+The place the hulk was champion in
+V... the army with the women
+Dr strange
+Scarlet Johansson 
+Black widow
+Hawkeye
+Vision
+Jarvis
+Iron mama lady friend
+Whatever plasma thing kept iron man alive?
+The amazing Spider-Man
+Toby Spider-Man
+The other Spider-Man
+Evil Spider-Man venom thing
+Dr octopus
+Green goblin
+The glider
+Radioactive spider
+God of mischief
+The king of Asgard
+13 MAY 19:04
+X-men
+Dr xavier 
+Gene gray
+Lava lady
+Cyclops
+Nightshade or something
+Teleporter Guy
+Storm
+Guy who freezes stuff
+Gf of guy who freezes stuff: energy drainer
+Blue doll
+Magneto
+Angel
+Wolverine / Logan / his other name
+Deadpool
+“Phoenix”
